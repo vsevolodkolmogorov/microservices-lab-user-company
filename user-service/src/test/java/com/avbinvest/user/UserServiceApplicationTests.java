d8b9151dcb48555c4877eb466eccac21298539a3
vilar <loadslowly@gmail.com>
2025-05-26 21:40:46 +0500

    feat: initial setup with Docker, Services, PostgreSQL

---
+++ user-service/src/test/java/com/avbinvest/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avbinvest.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
